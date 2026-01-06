c70137b2bc76929f30c269cfe26889a0c639801e
SKIND0 <skind@student.touro.edu>
2026-01-06 16:00:50 -0500

    done venue

--- src/main/java/edu/course/eventplanner/service/VenueSelector.java
+++ src/main/java/edu/course/eventplanner/service/VenueSelector.java
@@ -5,6 +5,32 @@ import java.util.*;
 
 public class VenueSelector {
     private final List<Venue> venues;
-    public VenueSelector(List<Venue> venues) { this.venues = venues; }
-    public Venue selectVenue(double budget, int guestCount) { return null; }
+    public VenueSelector(List<Venue> venues) {
+        this.venues = venues;
+    }
+
+    public Venue selectVenue(double budget, int guestCount) {
+        List<Venue> validVenues = new ArrayList<>();
+        for (Venue v : venues) {
+            if (v.getCost() <= budget && v.getCapacity() >= guestCount) {
+                validVenues.add(v);
+            }
+        }
+        if (validVenues.isEmpty()) {
+            return null;
+        }
+        Collections.sort(validVenues, new Comparator<Venue>() {
+            @Override
+            public int compare(Venue v1, Venue v2) {
+                if (v1.getCost() < v2.getCost()) {
+                    return -1;
+                } else if (v1.getCost() > v2.getCost()) {
+                    return 1;
+                } else {
+                    return Integer.compare(v1.getCapacity(), v2.getCapacity());
+                }
+            }
+        });
+        return validVenues.get(0);
+    }
 }
